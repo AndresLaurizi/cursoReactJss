e6c4f5cbae06d4e17d3b763d0aed6fa538ec66ec
Andres <andreslaurizi@hotmail.com>
2022-02-27 10:53:03 -0300

    NavBar

---
+++ reactAp/src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import './NavBar.css'
+
+function NavBar() {
+  return (
+    <div>
+        <h2>Costa y Motores</h2>
+        <ul>
+        <li><a href="#">Autos</a></li>
+        <li><a href="#">Camionetas</a></li>
+        <li><a href="#">Motos</a></li>
+        <li><a href="#">Of Road</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default NavBar

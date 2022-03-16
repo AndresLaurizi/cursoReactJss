5a4473e100066cfb4a3fc7045af21b7f5277d671
Andres <andreslaurizi@hotmail.com>
2022-03-16 10:23:39 -0300

    Contador

--- reactAp/src/App.js
+++ reactAp/src/App.js
@@ -4,12 +4,15 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './container/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
+
 
 function App() {
   return (
    <>
     <NavBar/>
     <ItemListContainer saludo = "hola soy ItemListConteiner"/>
+    <ItemCount stock= "5" inicial = "1" />
     <div className="App">
       <header className="App-header">
         <p>
---
+++ reactAp/src/components/ItemCount/ItemCount.js
@@ -0,0 +1,50 @@
+import './Contador.css'
+import React, { useEffect, useState } from 'react'
+
+
+export default function Contador({ stock, inicial }) {
+
+  const [contador, setCount] = useState(parseInt(inicial))
+  const [desactivadoBtn, setDesactivadoBtn] = useState(false);
+  const [stockNuevo, setStockNuevo] = useState(parseInt(stock))
+
+  const resta = () => {
+    if (contador === 1) {
+      alert("el minimo para ingresar es 1")
+    }
+    else {
+      setCount(contador - 1)
+    }
+  }
+  const suma = () => {
+    if (contador < stockNuevo) {
+      setCount(contador + 1)
+    } 
+  }
+
+  const agregar = () => {
+    setStockNuevo(stockNuevo - contador)
+  }
+
+ useEffect(() => {
+   if(stockNuevo===0){
+     alert("Sin stock")
+     setDesactivadoBtn(true)
+   }
+   setCount(parseInt(inicial))
+ },[stockNuevo,inicial])
+
+  return (
+    <>
+      <div class="botonera" >
+        <button onClick={resta} class="resta">-</button>
+        <p class="result">{contador}</p>
+        <button onClick={suma} class="suma">+</button>
+      </div>
+      <div>   
+      <button onClick= {agregar} disabled= {desactivadoBtn} >Agregar carrito  </button>
+      </div>
+    </>
+  )
+}
+
